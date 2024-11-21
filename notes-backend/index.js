3d92f6b1c3dc9b6b934bc9d6e9090beff1a5a824
larebear007 <larencozart@gmail.com>
2024-11-21 00:47:39 -0600

    Fix app.put logic, Add validation for .post and .put requests

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -57,11 +57,8 @@ app.delete("/api/notes/:id", (req, res, next) => {
       .catch(error => next(error));
 })
 
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
   const body = req.body;
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
 
   const note = Note({
     content: body.content,
@@ -69,20 +66,27 @@ app.post("/api/notes", (req, res) => {
   });
 
   note.save()
-      .then(savedNote => res.json(savedNote));
+      .then(savedNote => res.json(savedNote))
+      .catch(error => next(error))
 })
 
 app.put("/api/notes/:id", (req, res, next) => {
-  const body = req.body;
+  const { content, important } = req.body
 
-  const note = {
-    content: body.content,
-    important: body.important
-  }
+  // const note = {
+  //   content: body.content,
+  //   important: body.important
+  // }
 
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-      .then()
-      .catch(error => next(error));
+  Note.findByIdAndUpdate(
+    req.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -94,7 +98,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
